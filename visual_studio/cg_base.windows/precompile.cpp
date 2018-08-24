a871cbfebc8fb081a470d89c31327d692e987913
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2018-08-24 17:49:55 +0200

    tried to configure OpenGL ES application in Visual Studio, but it's a mess

---
+++ visual_studio/cg_base.windows/precompile.cpp
@@ -0,0 +1,8 @@
+// please_precompile.cpp : source file that includes just the standard includes
+// please_render.pch will be the pre-compiled header
+// please_precompile.obj will contain the pre-compiled type information
+
+#include "precompile.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
---
+++ visual_studio/cg_base.windows/precompile.h
@@ -0,0 +1,8 @@
+// please_precompile.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+#pragma once
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#include "cg_base.h"
