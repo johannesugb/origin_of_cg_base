38a731952b83b6c8b8bf9ce5b296f8a9d2ce7af1
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2019-07-17 18:22:47 +0200

    renamed to push_constants.h and added to .filters

--- framework/include/cg_base.h
+++ framework/include/cg_base.h
@@ -94,7 +94,7 @@
 #include "buffer_data.h"
 #include "buffer.h"
 #include "input_description.h"
-#include "push_constant_binding_data.h"
+#include "push_constants.h"
 #include "graphics_pipeline_config.h"
 
 #if defined(USE_OPENGL46_CONTEXT)
